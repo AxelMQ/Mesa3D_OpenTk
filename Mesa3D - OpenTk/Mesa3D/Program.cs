c7a6ef9b4d85ce60be7a5cb59b7031332e95d508
AxelMQ <75533726+AxelMQ@users.noreply.github.com>
2020-12-11 23:07:08 -0400

    Add files via upload

---
+++ Mesa3D - OpenTk/Mesa3D/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Mesa3D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindow window = new GameWindow(500, 500);
+            //window.Run(1.0 / 60.0);
+            Game gm = new Game(window);
+        }
+    }
+}
